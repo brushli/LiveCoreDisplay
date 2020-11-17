becff1829876b216d9f51c8824aeea08c0477db6
570738683@qq.com <570738683@qq.com>
2020-11-17 19:17:42 +0800

    全局异常处理，头像高宽的处理

---
+++ LiveCodeDisplayA/Attrubites/ExceptionHandleErrorAttribute .cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LiveCodeDisplayA.Attrubites
+{
+    public class ExceptionHandleErrorAttribute: HandleErrorAttribute
+    {
+        /// <summary>
+        /// 错误拦截
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext.ExceptionHandled)
+            {
+                return;
+            }
+            var result= new ContentResult();
+            result.Content = $"<h1>服务器出现错误，请稍后在扫码！</h1>";
+            filterContext.ExceptionHandled = true;
+            filterContext.Result = result;
+        }
+    }
+}
